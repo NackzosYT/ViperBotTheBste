f87cb5706321b6ce220737e45605a0f6c3fba5ba
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2019-01-30 02:15:31 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2838,13 +2838,7 @@ client.on('message', message => {
 ///end
 //////////////////
 
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
+
 /*
 البكجآت
 npm install discord.js
